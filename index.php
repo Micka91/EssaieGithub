9f9171f3db0a5a63002ed19ac7530e701792e442
Mickael CHRISTINE <mickael.christine@hotmail.com>
2019-04-23 16:29:39 +0200

    jquery

--- index.php
+++ index.php
@@ -4,7 +4,18 @@ require_once "function.php";
 $resultat = getAllAnnonces();
 
 ?>
-<ul>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="js/jquery-3.4.0.min.js"></script>
+<script src="js/script.js"></script>
+    <title>Document</title>
+</head>
+<body>
+    <ul>
     <li><?php echo $resultat["annonce1"][0] ?></li>
     <li><?php echo $resultat["annonce1"][1] ?></li>
 </ul>
@@ -13,6 +24,10 @@ $resultat = getAllAnnonces();
     <li><?php echo $resultat["annonce2"][0] ?></li>
     <li><?php echo $resultat["annonce2"][1] ?></li>
 </ul>
+</body>
+</html>
+
+
 <?php 
 
 $first = getFirstAnnonce();
