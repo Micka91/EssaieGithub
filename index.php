44f9936317b087993c6a404c6ee5103fb11ecf12
Mickael CHRISTINE <mickael.christine@hotmail.com>
2019-04-23 11:50:26 +0200

    afficher une deuxième annonce

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php 
+require_once "function.php";
+
+$resultat = getAllAnnonces();
+
+?>
+<ul>
+    <li><?php echo $resultat["annonce1"][0] ?></li>
+    <li><?php echo $resultat["annonce1"][1] ?></li>
+</ul>
